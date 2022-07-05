fe51152d8ab11afdef617d50d101602f1fdd5b8b
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2022-07-05 23:15:10 +0530

    update processor.v

--- Vivado Project/ImageDownSample.srcs/sources_1/new/Processor.v
+++ Vivado Project/ImageDownSample.srcs/sources_1/new/Processor.v
@@ -22,39 +22,63 @@
 
 module Processor(
     input en,
-    input clk
+    input clk,
+    output [17:0] data_addr; 
     );
 
+    // used in Instruction_Ram
+    wire [7:0] instruction_address;
+    wire [25:0] instruction_out_memory;
+    
+    // used in Control Unit
+    wire z_flag,complete;
+    wire [7:0] instruction_out_MBRU;
+    wire [35:0] control_signals;
+    
+    // used in MBRU
+    wire [27:0] data_out;
+
+    // used in MAR
+    wire [27:0] mar_out;
+    wire [27:0] mar_in;
+    
+
     output wire [7:0] instruction_out;
-    output wire [27:0] data_out;   
-    output wire [17:0] data_addr; 
+       
+    
     output wire [7:0] DRam_out; 
 
-    wire z_flag,complete,fetch,w_en,write_en,read_en,inc;
+    wire fetch,w_en,write_en,read_en,inc;
     wire [7:0] instr_address;
-    wire [7:0] MBRU;
-    wire [35:0] control_signals;
+    
+    
     wire [27:0] data_in,MBRU_out;
-    wire [7:0] instruction_address;
-    wire [25:0] instruction_in;
+    
+    
     wire [7:0] DRam_in;  
 
+    // INSTRUCTION RAM
+
+    module Instruction_Ram(
+    input clk,
+    input [7:0] address,
+    output reg [25:0] instr_out
+    );
+
+    Instruction_Ram IRAM(
+        .clk(clk),      // input clk
+        .address(instruction_address)           // to be initialized [7:0] address
+        .instr_out(instruction_out_memory)      // to be initialized reg [25:0] instr_out
+    );
+
+
     //Control_Unit C1(clk,en,z_flag,instr_address,MBRU,control_signals,complete);
-    // module Control_Unit(
-    // input clk,
-    // input en,
-    // input z_flag,
-    // input [7:0] instr_address,
-    // input [7:0] MBRU,
-    // output reg [35:0] control_signals,
-    // output complete
-    // );
     Control_Unit CU(
         .clk(clk),
         .en(en),
         .z_flag(z_flag),
-        .instr_address(instr_address),
-        .MBRU(MBRU),
+        .instr_address(control_signals[35:28]), // from bits 35 to 28(inclusive)
+        .MBRU(instruction_out_MBRU),
         .control_signals(control_signals),
         .complete(complete)
     );
@@ -62,16 +86,16 @@ module Processor(
     //MBRU MBRU1(.clk(clk),fetch,control_signals[18],instruction_in,MBRU,MBRU_out);
     MBRU MBRU(
         .clk(clk),
-        .fetch(fetch),
-        .instruction_in(instruction_in),
-        .instruction_out(instruction_out),
+        .fetch(control_signals[8]),
+        .instruction_in(instruction_out_memory),
+        .instruction_out(instruction_out_MBRU),
         .data_out(data_out)
     );
 
     MAR MAR(
         .clk(clk),
         .w_en(w_en),
-        .data_out(data_out),
+        .data_out(mar_out),
         .data_in(data_in),
         .data_addr(data_addr)
     );

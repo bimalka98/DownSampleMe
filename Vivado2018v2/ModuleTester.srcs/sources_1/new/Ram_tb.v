cc9e7a3d148b8b501fb0f889a77fd6bf974a1c9c
bimalka98 <bimalkapiyaruwan1998322@gmail.com>
2022-07-06 19:56:01 +0530

    created new test bench files for RAM and Register

---
+++ Vivado2018v2/ModuleTester.srcs/sources_1/new/Ram_tb.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Bimalka Piyaruwan
+// 
+// Create Date: 07/06/2022 07:49:17 PM
+// Design Name: 
+// Module Name: Ram_tb
+// Project Name: DownSampleMe
+// Target Devices: 
+// Tool Versions: 
+// Description: Test bench for Ram.v
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Ram_tb(); // no sensitivity list for test benches
+
+// signal declaration
+
+endmodule
---
+++ Vivado2018v2/ModuleTester.srcs/sources_1/new/Register_tb.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Bimalka Piyaruwan
+// 
+// Create Date: 07/06/2022 07:53:46 PM
+// Design Name: 
+// Module Name: Register_tb
+// Project Name: DownSampleMe
+// Target Devices: 
+// Tool Versions: 
+// Description: Test bench for Register.v
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Register_tb(); // no sensitivity list for test benches
+
+// signal declaration
+
+endmodule
